95b103adaa1dfe2d8f1342a28f4eb7056036332c
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-04-26 12:01:26 +0200

    gogo

--- philo_one/srcs/philo_think.c
+++ philo_one/srcs/philo_think.c
@@ -18,7 +18,5 @@ void	philo_think(t_info *infos, t_philo *philos)
 	pthread_mutex_lock(&infos->mutex_stdout);
 	printf("%6dms   %d   is thinking\n",
 	timer() - infos->time_ref, philos->id + 1);
-	if ((int)infos->current_nb_meal >= infos->nb_philos)
-		return ;
 	pthread_mutex_unlock(&infos->mutex_stdout);
 }
