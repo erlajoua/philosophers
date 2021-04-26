de1fbef8a7be5310be995bf11ff87d0a9d025d09
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-04-26 11:57:22 +0200

    gogo

--- philo_one/srcs/philo_sleep.c
+++ philo_one/srcs/philo_sleep.c
@@ -17,6 +17,8 @@ void	philo_sleep(t_info *infos, t_philo *philos)
 	//if (infos->onedead == 1 || (int)infos->current_nb_meal >= infos->nb_philos)
 	//	return ;
 	pthread_mutex_lock(&infos->mutex_stdout);
+	if ((int)infos->current_nb_meal >= infos->nb_philos)
+		return ;
 	printf("%6dms   %d   is sleeping\n",
 	timer() - infos->time_ref, philos->id + 1);
 	pthread_mutex_unlock(&infos->mutex_stdout);
