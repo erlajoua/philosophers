8df2c8c74b06f3b2c1ad9741891d907331dbd24b
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-04-27 11:23:45 +0200

    gogo

--- philo_two/srcs/init_forks.c
+++ philo_two/srcs/init_forks.c
@@ -16,6 +16,8 @@ void			init_forks(t_info *infos)
 {
 	sem_unlink("stdout");
 	sem_unlink("forks");
-	infos->sem_stdout = sem_open("stdout", O_CREAT | O_EXCL, S_IRWXU, 1);
-	infos->forks = sem_open("forks", O_CREAT | O_EXCL, S_IRWXU, infos->nb_philos);
+	infos->sem_stdout = sem_open("stdout", O_CREAT | O_EXCL,
+	S_IRWXU, 1);
+	infos->forks = sem_open("forks", O_CREAT | O_EXCL,
+	S_IRWXU, infos->nb_philos);
 }
